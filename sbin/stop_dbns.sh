c74df3c81dc8fac3e116fd5f452500a45906e3df
ShengjieLuo <luoshengjie@sjtu.edu.cn>
2017-01-10 15:33:56 +0800

    resolve the version conflict

--- sbin/stop_dbns.sh
+++ sbin/stop_dbns.sh
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 source env.rc
 sudo kill -9 `ps -aux | grep LogProducer | awk '{print $2}'`
 sudo kill -9 `ps -aux | grep LogStreaming | awk '{print $2}'`
 sudo kill -9 `ps -aux | grep run.py | awk '{print $2}'`
 python $DBNS_HOME/script/cleanSQL.py
-=======
-sudo kill -9 `ps -aux | grep LogProducer | awk '{print $2}'`
-sudo kill -9 `ps -aux | grep LogStreaming | awk '{print $2}'`
-sudo kill -9 `ps -aux | grep run.py | awk '{print $2}'`
-
->>>>>>> 868d9d087c1c9bc94ab695a81ccbd900ac17a1a3
