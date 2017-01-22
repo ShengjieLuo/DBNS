a736c1b21019e2ee674562c41ad5c8b73f6ce5e1
ShengjieLuo <luoshengjie@sjtu.edu.cn>
2017-01-22 13:18:22 +0800

    Migrate the environment to company

---
+++ proxy.sh
@@ -0,0 +1,5 @@
+export http_proxy=10.239.4.160:913
+export rsync_proxy=10.239.4.160:913
+export socket_proxy=10.239.4.160:913
+export https_proxy=10.239.4.160:913
+export no_proxy=*intel.com:911,127.0.0.1,192.168.*,10.*,localhost,10.1.1.53,10.1.1.55,10.1.1.59,10.1.1.61,ms0101,ms0102,ms0104,ms0105,172.16.0.59,172.16.0.68
