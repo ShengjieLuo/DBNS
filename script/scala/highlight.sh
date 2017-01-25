373dc51466889985d12c1dcda6069597c0c1c7cf
ShengjieLuo <luoshengjie@sjtu.edu.cn>
2017-01-25 18:49:34 +0800

    Scala Highlight

---
+++ script/scala/highlight.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.vim/{ftdetect,indent,syntax}
+cp $DBNS_HOME/script/scala/scala.vim.ftdetect ~/.vim/ftdetect/scala.vim
+cp $DBNS_HOME/script/scala/scala.vim.indent ~/.vim/indent/scala.vim
+cp $DBNS_HOME/script/scala/scala.vim.syntax ~/.vim/syntax/scala.vim
