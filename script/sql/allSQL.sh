9e97d725533798b7e5993dec82f9712730660bdc
ShengjieLuo <luoshengjie@sjtu.edu.cn>
2017-01-27 10:43:39 +0800

    Hive Standalone ready, see the script/sql/hiveSQL

--- script/sql/allSQL.sh
+++ script/sql/allSQL.sh
@@ -1,2 +1,4 @@
 # used for golbal sql configuration
 set global sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+update user set host = '%' where user = 'root';
+flush privileges;
