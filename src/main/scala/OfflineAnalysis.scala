432c2d97faec6a919a4208588d051ffe088315ef
shengjieluo <luoshengjie@sjtu.edu.cn>
2017-02-20 06:13:12 +0000

    compile

--- src/main/scala/OfflineAnalysis.scala
+++ src/main/scala/OfflineAnalysis.scala
@@ -24,6 +24,7 @@ def main(args:Array[String]){
   StreamingExamples.setStreamingLogLevels()
   val sc = new SparkConf().setAppName("LogStreamingAdvancedSparkCluster").setMaster("spark://172.16.0.104:7077")
   val sssc = new SparkContext(sc)
+  val ssc = new StreamingContext(sssc,Seconds(60))
   val sqlContext = new SQLContext(sssc)
   val hiveCtx = new HiveContext(sssc)
   val prop = new Properties()
@@ -33,28 +34,27 @@ def main(args:Array[String]){
   val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm") 
   val time:String = dateFormat.format(new Date())
   
-  val HRQipd = hiveCtx.sql("SELECT ipd,count(*) as count FROM hrq.original GROUP BY ipd having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
-  val HRQips = hiveCtx.sql("SELECT ips,count(*) as count FROM hrq.original GROUP BY ips having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
-  val HRQpd = hiveCtx.sql("SELECT pd,count(*) as count FROM hrq.original GROUP BY pd having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
-  val HRQps = hiveCtx.sql("SELECT ps,count(*) as count FROM hrq.original GROUP BY ps having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
+  val HRQipd = hiveCtx.sql("SELECT time,ipd,count(*) as count FROM hrq.original GROUP BY ipd having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val HRQips = hiveCtx.sql("SELECT time,ips,count(*) as count FROM hrq.original GROUP BY ips having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val HRQpd = hiveCtx.sql("SELECT time,pd,count(*) as count FROM hrq.original GROUP BY pd having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val HRQps = hiveCtx.sql("SELECT time,ps,count(*) as count FROM hrq.original GROUP BY ps having count > 100 ORDER BY count DESC LIMIT 50").rdd
     
-  val HRSipd = hiveCtx.sql("SELECT ipd,count(*) as count FROM hrs.original GROUP BY ipd having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
-  val HRSips = hiveCtx.sql("SELECT ips,count(*) as count FROM hrs.original GROUP BY ips having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
-  val HRSpd = hiveCtx.sql("SELECT pd,count(*) as count FROM hrs.original GROUP BY pd having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
-  val HRSps = hiveCtx.sql("SELECT ps,count(*) as count FROM hrs.original GROUP BY ps having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
-  val HRSrc = hiveCtx.sql("SELECT rc,count(*) as count FROM hrs.original GROUP BY rc having count > 100 ORDER BY count DESC LIMIT 50;").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
+  val HRSipd = hiveCtx.sql("SELECT time,ipd,count(*) as count FROM hrs.original GROUP BY ipd having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val HRSips = hiveCtx.sql("SELECT time,ips,count(*) as count FROM hrs.original GROUP BY ips having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val HRSpd = hiveCtx.sql("SELECT time,pd,count(*) as count FROM hrs.original GROUP BY pd having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val HRSps = hiveCtx.sql("SELECT time,ps,count(*) as count FROM hrs.original GROUP BY ps having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val HRSrc = hiveCtx.sql("SELECT time,rc,count(*) as count FROM hrs.original GROUP BY rc having count > 100 ORDER BY count DESC LIMIT 50;").rdd
   
-  val DRQips = hiveCtx.sql("SELECT ips,count(*) as count FROM drq.original GROUP BY ips having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
-  val DRQipd = hiveCtx.sql("SELECT ipd,count(*) as count FROM drq.original GROUP BY ipd having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
-  val DRQname = hiveCtx.sql("SELECT name,count(*) as count FROM drq.original GROUP BY name having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
-  val DRQtype = hiveCtx.sql("SELECT type,count(*) as count FROM drq.original GROUP BY type having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val DRQips = hiveCtx.sql("SELECT time,ips,count(*) as count FROM drq.original GROUP BY ips having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val DRQipd = hiveCtx.sql("SELECT time,ipd,count(*) as count FROM drq.original GROUP BY ipd having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val DRQname = hiveCtx.sql("SELECT time,name,count(*) as count FROM drq.original GROUP BY name having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val DRQtype = hiveCtx.sql("SELECT time,type,count(*) as count FROM drq.original GROUP BY type having count > 100 ORDER BY count DESC LIMIT 50").rdd
 
-  
-  val DRSips = hiveCtx.sql("SELECT ips,count(*) as count FROM drs.original GROUP BY ips having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
-  val DRSipd = hiveCtx.sql("SELECT ipd,count(*) as count FROM drs.original GROUP BY ipd having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
-  val DRSname = hiveCtx.sql("SELECT name,count(*) as count FROM drs.original GROUP BY name having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt)) 
-  val DRStype = hiveCtx.sql("SELECT type,count(*) as count FROM drs.original GROUP BY type having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
-  val DRSurl = hiveCtx.sql("SELECT url,count(*) as count FROM drs.original GROUP BY url having count > 100 ORDER BY count DESC LIMIT 50").rdd.map(p => Row(time,p._2.trim,p._1.toInt))
+  val DRSips = hiveCtx.sql("SELECT time,ips,count(*) as count FROM drs.original GROUP BY ips having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val DRSipd = hiveCtx.sql("SELECT time,ipd,count(*) as count FROM drs.original GROUP BY ipd having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val DRSname = hiveCtx.sql("SELECT time,name,count(*) as count FROM drs.original GROUP BY name having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val DRStype = hiveCtx.sql("SELECT time,type,count(*) as count FROM drs.original GROUP BY type having count > 100 ORDER BY count DESC LIMIT 50").rdd
+  val DRSurl = hiveCtx.sql("SELECT time,url,count(*) as count FROM drs.original GROUP BY url having count > 100 ORDER BY count DESC LIMIT 50").rdd
 
   val ipsschema = StructType(List(StructField("id",StringType,true),StructField("IPSource",StringType,true),StructField("count",IntegerType,true)))
   val ipdschema = StructType(List(StructField("id",StringType,true),StructField("IPDest",StringType,true),StructField("count",IntegerType,true)))
@@ -65,24 +65,24 @@ def main(args:Array[String]){
   val urlschema  = StructType(List(StructField("id",StringType,true),StructField("url",StringType,true),StructField("count",IntegerType,true)))
   val rcschema  = StructType(List(StructField("id",StringType,true),StructField("returnCode",StringType,true),StructField("count",IntegerType,true)))
 
-  sqlContext.createDataFrame(IPSourceTop,ipsschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRQips", prop)
-  sqlContext.createDataFrame(PortSourceTop,psschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRQps", prop)
-  sqlContext.createDataFrame(IPDestTop,ipdschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRQipd", prop)
-  sqlContext.createDataFrame(PortDestTop,pdschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRQpd", prop)
-  sqlContext.createDataFrame(IPSourceTop,ipsschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRSips", prop)
-  sqlContext.createDataFrame(PortSourceTop,psschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRSps", prop)
-  sqlContext.createDataFrame(IPDestTop,ipdschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRSipd", prop)
-  sqlContext.createDataFrame(PortDestTop,pdschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRSpd", prop)
-  sqlContext.createDataFrame(rcTop,rcschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRSrc", prop)
-  sqlContext.createDataFrame(IPSourceTop,ipsschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRQips", prop)
-  sqlContext.createDataFrame(IPDestTop,ipdschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRQipd", prop)
-  sqlContext.createDataFrame(nameTop,nameschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRQname", prop)
-  sqlContext.createDataFrame(typeTop,typeschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRQtype", prop)
-  sqlContext.createDataFrame(IPSourceTop,ipsschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRSips", prop)
-  sqlContext.createDataFrame(IPDestTop,ipdschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRSipd", prop)
-  sqlContext.createDataFrame(nameTop,nameschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRSname", prop)
-  sqlContext.createDataFrame(typeTop,typeschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRStype", prop)
-  sqlContext.createDataFrame(urlTop,urlschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRSurl", prop)
+  sqlContext.createDataFrame(HRQips,ipsschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRQips", prop)
+  sqlContext.createDataFrame(HRQps,psschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRQps", prop)
+  sqlContext.createDataFrame(HRQipd,ipdschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRQipd", prop)
+  sqlContext.createDataFrame(HRQpd,pdschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRQpd", prop)
+  sqlContext.createDataFrame(HRSips,ipsschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRSips", prop)
+  sqlContext.createDataFrame(HRSps,psschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRSps", prop)
+  sqlContext.createDataFrame(HRSipd,ipdschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRSipd", prop)
+  sqlContext.createDataFrame(HRSpd,pdschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRSpd", prop)
+  sqlContext.createDataFrame(HRSrc,rcschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.HRSrc", prop)
+  sqlContext.createDataFrame(DRQips,ipsschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRQips", prop)
+  sqlContext.createDataFrame(DRQipd,ipdschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRQipd", prop)
+  sqlContext.createDataFrame(DRQname,nameschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRQname", prop)
+  sqlContext.createDataFrame(DRQtype,typeschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRQtype", prop)
+  sqlContext.createDataFrame(DRSips,ipsschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRSips", prop)
+  sqlContext.createDataFrame(DRSipd,ipdschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRSipd", prop)
+  sqlContext.createDataFrame(DRSname,nameschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRSname", prop)
+  sqlContext.createDataFrame(DRStype,typeschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRStype", prop)
+  sqlContext.createDataFrame(DRSurl,urlschema).write.mode("append").jdbc("jdbc:mysql://172.16.0.104:3306/DBNS", "DBNS.DRSurl", prop)
 
   //Step final: start the spark streaming context
   ssc.start
