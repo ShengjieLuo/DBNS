f084cb2c3dace54204667fb6fe58c068deb3822f
ShengjieLuo <luoshengjie@sjtu.edu.cn>
2017-02-20 17:22:23 +0800

    Offline Analysis in Spark Scala Framework

---
+++ run/runOffline.sh
@@ -0,0 +1,5 @@
+/usr/local/spark/bin/spark-submit \
+--class "OfflineAnalysis" \
+--executor-cores 6 \
+--master spark://spark-master:7077 \
+/usr/local/DBNS/target/scala-2.10/simple-project_2.10-1.0.jar
