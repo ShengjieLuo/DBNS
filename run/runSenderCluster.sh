d5ef4f01308870206be9309a52493bb1e02a3a02
ShengjieLuo <luoshengjie@sjtu.edu.cn>
2017-03-09 21:32:05 +0800

    flask interface built from DNS mysql database

--- run/runSenderCluster.sh
+++ run/runSenderCluster.sh
@@ -5,7 +5,7 @@
 --class "LogProducer_1" \
 --total-executor-cores 10 \
 /usr/local/DBNS/target/scala-2.10/simple-project_2.10-1.0.jar \
-172.16.0.104:9092 0 \
+172.16.0.104:9092 2 \
 /usr/local/sample/http_response.txt \
 /usr/local/sample/http_request.txt \
 /usr/local/sample/dns_response.txt.20M \
@@ -18,7 +18,7 @@
 --class "LogProducer_2" \
 --total-executor-cores 10 \
 /usr/local/DBNS/target/scala-2.10/simple-project_2.10-1.0.jar \
-172.16.0.104:9093 0 \
+172.16.0.104:9093 2 \
 /usr/local/sample/netflow.txt \
 /usr/local/sample/natlog.txt \
 /usr/local/sample/syslog.txt &
--- server/server.py
+++ server/server.py
@@ -16,18 +16,18 @@ app.config['MYSQL_DATABASE_HOST'] = 'spark-master'
 
 mysql.init_app(app)
 
-@app.route('/<table>/<famliy>/<start>/<end>')
+@app.route('/<name>/<famliy>/<start>/<end>')
 def showbookname(name,famliy,start,end):
 	cursor = mysql.get_db().cursor()
 
 	# Stage1: Verfication
 	if name == "http_request" or name == "http_response" or name == "dns_response" or name == "dns_request":
-		continue
+		pass
 	else:
 		return render_template('test.html', obj = "Wrong Table Name")
 
 	if famliy == "top10-1h" or famliy == "throughput":
-		continue
+		pass
 	else:
 		return render_template('test.html', obj = "Wrong Statistical Item!")
 	
@@ -47,19 +47,127 @@ def showbookname(name,famliy,start,end):
 	elif name == "dns_request":
 		sqltable = "DRQ"
 
-	sqlstart = 2600
-	if (start+3600-start%3600<end):
-		a = start+3600-start%3600
-		sqltime.append(a)
-		while a+3600 < b:
-			a = a+ 3600
-			sqltime.append(a)
+	#sqlstart = 2600
+        sqlstart = start+3600-start%3600
+        sqltime = []
+	if (sqlstart<end):
+		sqltime.append(str(sqlstart))
+		sqlnew = sqlstart
+		while sqlnew+3600 < end:
+			sqlnew = sqlnew + 3600
+			sqltime.append(str(sqlnew))
+	print sqltime
 	
-	
-	cursor.execute("SELECT * FROM DBNS.DRQips WHERE id<")
-	testobj = cursor.fetchall()
-	print testobj
-	obj = "Info:"+str(testobj[0][0])+" name:"+str(testobj[0][1])+" count:"+str(testobj[0][2])
+	DRQips , DRQipd , DRQurl = [],[],[]
+	DRSips , DRSipd , DRSurl = [],[],[]
+	HRQips , HRQipd , HRQipss , HRQipds = [],[],[],[]
+	HRSips , HRSipd , HRSipss , HRSipds = [],[],[],[]
+	DRQcount , DRScount =[],[]
+	HRQcount , HRQsize = [],[]
+	HRScount , HRSsize = [],[]
+
+	for time in sqltime:
+		print name,famliy
+		if name == "http_request" and famliy == "top10-1h":
+			command = "SELECT * FROM DBNS."+sqltable+"ips WHERE time= "+time+" LIMIT 10"
+			print command
+			cursor.execute(command)
+			HRQips.append(cursor.fetchall())
+			
+			command = "SELECT * FROM DBNS."+sqltable+"ipd WHERE time= "+time+" LIMIT 10"
+			print command
+			cursor.execute(command)
+			HRQipd.append(cursor.fetchall())
+			
+			command = "SELECT * FROM DBNS."+sqltable+"ipss WHERE time= "+time+" LIMIT 10"
+			print command
+			cursor.execute(command)
+			HRQipss.append(cursor.fetchall())
+			
+			command = "SELECT * FROM DBNS."+sqltable+"ipds WHERE time= "+time+" LIMIT 10"
+			print command
+			cursor.execute(command)
+			HRQipds.append(cursor.fetchall())
+		
+		elif name == "http_response" and famliy == "top10-1h":
+			command = "SELECT * FROM DBNS."+sqltable+"ips WHERE time= "+time+" LIMIT 10"
+			cursor.execute(command)
+			HRSips.append(cursor.fetchall())
+			
+			command = "SELECT * FROM DBNS."+sqltable+"ipd WHERE time= "+time+" LIMIT 10"
+			cursor.execute(command)
+			HRSipd.append(cursor.fetchall())
+			
+			command = "SELECT * FROM DBNS."+sqltable+"ipss WHERE time= "+time+" LIMIT 10"
+			cursor.execute(command)
+			HRSipss.append(cursor.fetchall())
+			
+			command = "SELECT * FROM DBNS."+sqltable+"ipds WHERE time= "+time+" LIMIT 10"
+			cursor.execute(command)
+			HRSipds.append(cursor.fetchall())
+		
+		elif name == "dns_response" and famliy == "top10-1h":
+			command = "SELECT * FROM DBNS."+sqltable+"ips WHERE time= "+time+" LIMIT 10"
+			cursor.execute(command)
+			DRSips.append(cursor.fetchall())
+		
+			command = "SELECT * FROM DBNS."+sqltable+"ipd WHERE time= "+time+" LIMIT 10"
+			cursor.execute(command)
+			DRSipd.append(cursor.fetchall())
+			
+			command = "SELECT * FROM DBNS."+sqltable+"name WHERE time= "+time+" LIMIT 10"
+			cursor.execute(command)
+			DRSurl.append(cursor.fetchall())
+
+		elif name == "dns_request" and famliy == "top10-1h":
+			command = "SELECT * FROM DBNS."+sqltable+"ips WHERE time= "+time+" LIMIT 10"
+			cursor.execute(command)
+			DRQips.append(cursor.fetchall())
+		
+			command = "SELECT * FROM DBNS."+sqltable+"ipd WHERE time= "+time+" LIMIT 10"
+			cursor.execute(command)
+			DRQipd.append(cursor.fetchall())
+			
+			command = "SELECT * FROM DBNS."+sqltable+"name WHERE time= "+time+" LIMIT 10"
+			cursor.execute(command)
+			DRQurl.append(cursor.fetchall())
+		
+		elif name == "http_request:" and famliy == "throughput":
+			command = "SELECT * FROM DBNS."+sqltable+"count WHERE time= "+time+" LIMIT 10"
+			cursor.execute(command)
+			HRQcount.append(cursor.fetchall())
+			
+			command = "SELECT * FROM DBNS."+sqltable+"sum WHERE time= "+time+" LIMIT 10"
+			cursor.execute(command)
+			HRQsum.append(cursor.fetchall())
+			
+		elif name == "http_response:" and famliy == "throughput":
+			command = "SELECT * FROM DBNS."+sqltable+"count WHERE time= "+time
+			cursor.execute(command)
+			HRScount.append(cursor.fetchall())
+			
+			command = "SELECT * FROM DBNS."+sqltable+"sum WHERE time= "+time
+			cursor.execute(command)
+			HRSsum.append(cursor.fetchall())
+					
+		elif name == "dns_response:" and famliy == "throughput":
+			command = "SELECT * FROM DBNS."+sqltable+"count WHERE time= "+time
+			cursor.execute(command)
+			DRScount.append(cursor.fetchall())
+
+		elif name == "dns_request:" and famliy == "throughput":
+			command = "SELECT * FROM DBNS."+sqltable+"count WHERE time= "+time
+			cursor.execute(command)
+			DRQcount.append(cursor.fetchall())
+
+	'''Debug
+	print "DRQ",DRQips,DRQipd,DRQurl,DRQcount
+	print "DRS",DRSips,DRSipd,DRSurl,DRScount
+        print "HRQ",HRQips,HRQipd,HRQipss,HRQipds,HRQcount,HRQsize
+        print "HRS",HRSips,HRSipd,HRSipss,HRSipds,HRScount,HRSsize
+	'''	
+
+	obj = "name:"+str(name)+" famliy:"+str(famliy)+" start:"+str(start)+" end:"+str(end)+"\n"
 	return render_template('test.html', obj = obj)
 
 if __name__ == '__main__':
